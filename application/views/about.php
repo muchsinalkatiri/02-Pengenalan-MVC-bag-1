ed0fa6a418eaca0ecec66280f2dd1fe3850aece4
Muchsin Alkatiri <muchsinalkatiri36@yahoo.com>
2018-03-26 16:52:02 +0700

    menambah search dan ubah warna

--- application/views/about.php
+++ application/views/about.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>HOME</title>
+		<title>About</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'Asset/css/bootstrap.min.css'; ?>">
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'Asset/css/kustom.css'; ?>">
 		<script type="text/javascript" src="<?php echo base_url().'Asset/js/bootstrap.min.js'; ?>"></script>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light mk-navbar">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
 			<a class="navbar-brand" href="#">MUCHSIN</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -20,6 +20,10 @@
 					<li class="nav-item">
 						<a class="nav-link" href="#">Contact</a>
 					</li>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline my-2 my-sm-0" type="submit">Search</button>
+					</form>
 				</ul>
 				<form class="form-inline my-2 my-lg-0">
 					<ul class="navbar-nav mr-auto nav nav-pills">
